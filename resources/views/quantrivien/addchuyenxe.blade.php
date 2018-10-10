231240abf5af0ec2f885029c98bd6c7d18a34ded
ADarkM94 <phananhminh51202164@gmail.com>
2018-10-10 18:43:15 +0700

    Update resources/views/quantrivien/addchuyenxe.blade.php

--- resources/views/quantrivien/addchuyenxe.blade.php
+++ resources/views/quantrivien/addchuyenxe.blade.php
@@ -78,178 +78,182 @@
         }
         option[2].classList.add('selected');
         option[2].getElementsByTagName('img')[0].setAttribute('src','{{asset("images/icons/chuyenxe-hover.png")}}');
-        $(function () {
-            var obj = {
-                width: '100%',
-                height: 480,
-                showTop: false,
-                showBottom: false,
-                collapsible: false,
-                showHeader: true,
-                filterModel: {on: true, mode: "AND", header: true},
-                scrollModel: {autoFit: true},
-                resizable: false,
-                roundCorners: false,
-                rowBorders: false,
-                columnBorders: false,
-                postRenderInterval: -1,
-                selectionModel: { type: 'row', mode: 'single' },
-                numberCell: { show: false },
-                stripeRows: false,
-                cellDblClick: function (event,ui) {
-                    window.open("{{url('/admin/addnhanvien')}}" + "/" + ui.rowData["Mã"]);
-                }
-            };
-            obj.colModel = [
-                {
-                    title: "ID",
-                    width: 50,
-                    dataIndx: "Mã",
-                    dataType: "string",
-                    editor: false,
-                    align: 'center',
-                    filter: {
-                        type: 'textbox',
-                        condition: 'contain',
-                        listeners: ['keyup']
+    </script>
+    @isset($ttchuyenxe)
+        <script>
+            $(function () {
+                var obj = {
+                    width: '100%',
+                    height: 480,
+                    showTop: false,
+                    showBottom: false,
+                    collapsible: false,
+                    showHeader: true,
+                    filterModel: {on: true, mode: "AND", header: true},
+                    scrollModel: {autoFit: true},
+                    resizable: false,
+                    roundCorners: false,
+                    rowBorders: false,
+                    columnBorders: false,
+                    postRenderInterval: -1,
+                    selectionModel: { type: 'row', mode: 'single' },
+                    numberCell: { show: false },
+                    stripeRows: false,
+                    cellDblClick: function (event,ui) {
+                        window.open("{{url('/admin/addnhanvien')}}" + "/" + ui.rowData["Mã"]);
                     }
-                },
-                {
-                    title: "Mã chuyến xe",
-                    width: 50,
-                    dataIndx: "Mã_chuyến_xe",
-                    dataType: "string",
-                    editor: false,
-                    align: 'center',
-                    filter: {
-                        type: 'textbox',
-                        condition: 'contain',
-                        listeners: ['keyup']
-                    }
-                },
-                {
-                    title: "Nhân viên đặt",
-                    width: 100,
-                    dataIndx: "Nhân_viên_đặt",
-                    dataType: "string",
-                    editor: false,
-                    align: 'center',
-                    filter: {
-                        type: 'textbox',
-                        condition: 'contain',
-                        listeners: ['keyup']
-                    }
-                },
-                {
-                    title: "Mã Khách Hàng",
-                    width: 150,
-                    dataIndx: "Mã_khách_hàng",
-                    dataType: "string",
-                    editor: false,
-                    align: 'center',
-                    filter: {
-                        type: 'textbox',
-                        condition: 'contain',
-                        listeners: ['keyup']
-                    }
-                },
-                {
-                    title: "Vị trí ghế",
-                    width: 100,
-                    dataIndx: "Vị_trí_ghế",
-                    dataType: "string",
-                    editor: false,
-                    align: "center",
-                    filter: {
-                        type: 'textbox',
-                        condition: 'contain',
-                        listeners: ['keyup']
-                    }
-                },
-                {
-                    title: "Giá",
-                    width: 200,
-                    dataIndx: "Giá",
-                    dataType: "string",
-                    editor: false,
-                    align: 'center',
-                    filter: {
-                        type: 'textbox',
-                        condition: 'contain',
-                        listeners: ['keyup']
-                    }
-                },
-                {
-                    title: "Trạng thái",
-                    width: 100,
-                    dataIndx: "Trạng_thái",
-                    dataType: "string",
-                    editor: false,
-                    align: 'center',
-                    render: function(ui){
-                        switch(ui.rowData["Trạng_thái"]){
-                            case 0:
-                                return 'Waiting';
-                                break;
-                            case 1:
-                                return 'Booked';
-                                break;
-                            case 2:
-                                return 'Completed';
-                                break;
+                };
+                obj.colModel = [
+                    {
+                        title: "ID",
+                        width: 50,
+                        dataIndx: "Mã",
+                        dataType: "string",
+                        editor: false,
+                        align: 'center',
+                        filter: {
+                            type: 'textbox',
+                            condition: 'contain',
+                            listeners: ['keyup']
                         }
                     },
-                    filter: {
-                        type: 'select',
-                        condition: 'equal',
-                        options: [
-                            {'':'All'},
-                            {'0':'Waiting'},
-                            {'1':'Booked'},
-                            {'2':'Completed'}
-                        ],
-                        listeners: ['change']
-                    }
-                },
-                {
-                    title: "Action",
-                    width: 100,
-                    editor: false,
-                    dataIndx: "View",
-                    render: function (ui) {
-                        var str = '';
-                        str += '<a title="Edit" id="idEditEmployee" ><i class="glyphicon glyphicon-edit  text-success" style="padding-right: 5px; cursor: pointer;"></i></a>';
-                        str += '<a title="Delete" id="idDelEmployee" ><i class="glyphicon glyphicon-remove  text-danger" style="padding-right: 5px; cursor: pointer;"></i></a>';
-                        return str;
+                    {
+                        title: "Mã chuyến xe",
+                        width: 50,
+                        dataIndx: "Mã_chuyến_xe",
+                        dataType: "string",
+                        editor: false,
+                        align: 'center',
+                        filter: {
+                            type: 'textbox',
+                            condition: 'contain',
+                            listeners: ['keyup']
+                        }
                     },
-                    postRender: function (ui) {
-                        var rowData = ui.rowData,
-                            $cell = this.getCell(ui);
-                        //add button
-                        $cell.find("a#idEditEmployee")
-                            .unbind("click")
-                            .bind("click", function (evt) {
-                                window.open("{{url('admin/addnhanvien')}}"+"/"+rowData["Mã"]);
-                            });
-                        $cell.find("a#idDelEmployee")
-                            .unbind("click")
-                            .bind("click", function (evt) {
-                                if(confirm("Bạn chắc chắn muốn xóa?"))
-                                    location.assign("{{url('admin/delnhanvien')}}"+"/"+rowData["Mã"]);
-                            });
+                    {
+                        title: "Nhân viên đặt",
+                        width: 100,
+                        dataIndx: "Nhân_viên_đặt",
+                        dataType: "string",
+                        editor: false,
+                        align: 'center',
+                        filter: {
+                            type: 'textbox',
+                            condition: 'contain',
+                            listeners: ['keyup']
+                        }
+                    },
+                    {
+                        title: "Mã Khách Hàng",
+                        width: 150,
+                        dataIndx: "Mã_khách_hàng",
+                        dataType: "string",
+                        editor: false,
+                        align: 'center',
+                        filter: {
+                            type: 'textbox',
+                            condition: 'contain',
+                            listeners: ['keyup']
+                        }
+                    },
+                    {
+                        title: "Vị trí ghế",
+                        width: 100,
+                        dataIndx: "Vị_trí_ghế",
+                        dataType: "string",
+                        editor: false,
+                        align: "center",
+                        filter: {
+                            type: 'textbox',
+                            condition: 'contain',
+                            listeners: ['keyup']
+                        }
+                    },
+                    {
+                        title: "Giá",
+                        width: 200,
+                        dataIndx: "Giá",
+                        dataType: "string",
+                        editor: false,
+                        align: 'center',
+                        filter: {
+                            type: 'textbox',
+                            condition: 'contain',
+                            listeners: ['keyup']
+                        }
+                    },
+                    {
+                        title: "Trạng thái",
+                        width: 100,
+                        dataIndx: "Trạng_thái",
+                        dataType: "string",
+                        editor: false,
+                        align: 'center',
+                        render: function(ui){
+                            switch(ui.rowData["Trạng_thái"]){
+                                case 0:
+                                    return 'Waiting';
+                                    break;
+                                case 1:
+                                    return 'Booked';
+                                    break;
+                                case 2:
+                                    return 'Completed';
+                                    break;
+                            }
+                        },
+                        filter: {
+                            type: 'select',
+                            condition: 'equal',
+                            options: [
+                                {'':'All'},
+                                {'0':'Waiting'},
+                                {'1':'Booked'},
+                                {'2':'Completed'}
+                            ],
+                            listeners: ['change']
+                        }
+                    },
+                    {
+                        title: "Action",
+                        width: 100,
+                        editor: false,
+                        dataIndx: "View",
+                        render: function (ui) {
+                            var str = '';
+                            str += '<a title="Edit" id="idEditEmployee" ><i class="glyphicon glyphicon-edit  text-success" style="padding-right: 5px; cursor: pointer;"></i></a>';
+                            str += '<a title="Delete" id="idDelEmployee" ><i class="glyphicon glyphicon-remove  text-danger" style="padding-right: 5px; cursor: pointer;"></i></a>';
+                            return str;
+                        },
+                        postRender: function (ui) {
+                            var rowData = ui.rowData,
+                                $cell = this.getCell(ui);
+                            //add button
+                            $cell.find("a#idEditEmployee")
+                                .unbind("click")
+                                .bind("click", function (evt) {
+                                    window.open("{{url('admin/addnhanvien')}}"+"/"+rowData["Mã"]);
+                                });
+                            $cell.find("a#idDelEmployee")
+                                .unbind("click")
+                                .bind("click", function (evt) {
+                                    if(confirm("Bạn chắc chắn muốn xóa?"))
+                                        location.assign("{{url('admin/delnhanvien')}}"+"/"+rowData["Mã"]);
+                                });
+                        }
                     }
-                }
-            ];
+                ];
 
-            obj.dataModel = {
-                data: {!! json_encode($ticket) !!},
-                location: "local",
-                sorting: "local",
-                sortDir: "down"
-            };
-            obj.pageModel = {type: 'local', rPP: 10, rPPOptions: [10, 20, 30, 50]};
-            var $grid = $("#ticket").pqGrid(obj);
-            $grid.pqGrid("refreshDataAndView");
-        });
-    </script>
+                obj.dataModel = {
+                    data: {!! json_encode($ticket) !!},
+                    location: "local",
+                    sorting: "local",
+                    sortDir: "down"
+                };
+                obj.pageModel = {type: 'local', rPP: 10, rPPOptions: [10, 20, 30, 50]};
+                var $grid = $("#ticket").pqGrid(obj);
+                $grid.pqGrid("refreshDataAndView");
+            });
+        </script>
+    @endisset
 @endsection
