b700de9ab8f3098db6cf727d537d2dab9e39b0b8
ADarkM94 <phananhminh51202164@gmail.com>
2018-10-11 19:23:34 +0700

    update

--- resources/views/quantrivien/thongke.blade.php
+++ resources/views/quantrivien/thongke.blade.php
@@ -70,6 +70,9 @@
 @section('excontent')
 @endsection
 @section('script')
+    {{--<script async="" src="http://www.google-analytics.com/analytics.js"></script>
+    <script src="{{asset('plugins/chartjs/Chart.bundle.js')}}"></script>
+    <script src="{{asset('plugins/chartjs/utils.js')}}"></script>--}}
     <script>
         document.getElementById("bieudo1").width=document.getElementById("bieudo1").parentNode.clientWidth - 15;
         document.getElementById("bieudo1").height=document.getElementById("bieudo1").parentNode.clientHeight - 15;
@@ -100,10 +103,66 @@
                     data : [203,156,99,251,305,247]
                 }
             ]
-        }
+        };
         var bieudo1 = document.getElementById('bieudo1').getContext('2d');
         var bieudo2 = document.getElementById('bieudo2').getContext('2d');
         new Chart(bieudo1).Line(buyerData);
         new Chart(bieudo2).Bar(buyerData);
+        /*var lineChartData = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [{
+                label: 'My First dataset',
+                borderColor: 'red',
+                backgroundColor: 'red',
+                fill: false,
+                data: [
+                   10,20,15,50,20,30,26
+                ],
+                yAxisID: 'y-axis-1',
+            }, {
+                label: 'My Second dataset',
+                borderColor: 'blue',
+                backgroundColor: 'blue',
+                fill: false,
+                data: [
+                    5,30,21,56,20,10,5
+                ],
+                yAxisID: 'y-axis-2'
+            }]
+        };
+
+        window.onload = function() {
+            var ctx = document.getElementById('bieudo2').getContext('2d');
+            window.myLine = Chart.Line(ctx, {
+                data: lineChartData,
+                options: {
+                    responsive: true,
+                    hoverMode: 'index',
+                    stacked: false,
+                    title: {
+                        display: true,
+                        text: 'Chart.js Line Chart - Multi Axis'
+                    },
+                    scales: {
+                        yAxes: [{
+                            type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+                            display: true,
+                            position: 'left',
+                            id: 'y-axis-1',
+                        }, {
+                            type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+                            display: true,
+                            position: 'right',
+                            id: 'y-axis-2',
+
+                            // grid line settings
+                            gridLines: {
+                                drawOnChartArea: false, // only want the grid lines for one axis to show up
+                            },
+                        }],
+                    }
+                }
+            });
+        };*/
     </script>
 @endsection
